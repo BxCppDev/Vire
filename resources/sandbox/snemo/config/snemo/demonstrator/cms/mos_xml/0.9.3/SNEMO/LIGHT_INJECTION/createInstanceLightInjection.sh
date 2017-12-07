37d1e02c86b00f33798d680ca3f1fe720ba2b513
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-12-07 16:24:47 +0100

    [sandbox/snemo] Add preliminary 0.9.3 XML files

---
+++ resources/sandbox/snemo/config/snemo/demonstrator/cms/mos_xml/0.9.3/SNEMO/LIGHT_INJECTION/createInstanceLightInjection.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+usage() {
+        echo "(script) ******************************"
+        echo "(script) *   Error parameter missing    "
+        echo "(script) *   Usage :                    "
+        echo "(script) *   ./createInstanceLightInjection.sh <InstanceNumber>"
+        echo "(script) *   <InstanceNumber>    -->  instance number allow to build the name of the xml file for this instance"
+        echo "(script) *******************************"
+}
+
+echo "create an instance of the Model : start"
+if [ "$1" == "" ]
+then
+        usage
+        exit
+fi
+
+
+m_name=LIGHT_INJECTION_$1.xml
+m_modelName=LIGHT_INJECTIONModel.xml
+NOW=$(date +"%d-%m-%Y %k:%M")
+sed -re "s/@Name@/$m_name/"  $m_modelName > temp.xml
+sed -re "s/@DateCreation@/$NOW/"  temp.xml > $m_name
+rm -rf temp.xml
+
+echo "create an instance ($m_name) with the Model($m_modelName) : finished"
